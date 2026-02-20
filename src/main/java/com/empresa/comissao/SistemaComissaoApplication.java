7f4540617dbb931a09a91364dae4fd87bd41870f
alexandreDinis <dinisde@gmail.com>
2026-02-20 15:22:57 -0300

    configura @EnableCaching na classe SistemaComissaoApplication.java para resolver a sobrecarga no banco

--- src/main/java/com/empresa/comissao/SistemaComissaoApplication.java
+++ src/main/java/com/empresa/comissao/SistemaComissaoApplication.java
@@ -2,8 +2,10 @@ package com.empresa.comissao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class SistemaComissaoApplication {
 
 	public static void main(String[] args) {
